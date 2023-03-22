54078221db76197b01fc0869ceae1bcf3365aa67
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-03-22 19:10:51 +0100

    add construct + instances

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <?php 
+
   class Movie {
-  public $title;
-  public $director;
-  public $year;
-  public $genre;
+    public $title;
+    public $director;
+    public $year;
+    public $genre;
+    
+    public function __construct($_title, $_director, $_year, $_genre)
+    {
+      $this->title = $_title;
+      $this->director = $_director;
+      $this->year = $_year; 
+      $this->genre = $_genre;
+    }
   }
   
-?>
+  $movie_1 = new Movie("Il Signore degli Anelli - Il ritorno del re","Peter Jackson","2003","fantasy");
+  
+  $movie_2 = new Movie("Dal tramonto all'alba","Quentin Tarantino" ,"1996","horror");
+ 
+  var_dump($movie_1);
+  var_dump($movie_2);
